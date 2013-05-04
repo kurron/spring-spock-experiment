45df589f4f2176bba28d668dedb9cc02a0783f96
kurron <9gnp2a8egj>
2013-05-04 17:24:17 -0400

    Proved that both the parent and child classes can have their own Spring context file.

--- src/test/groovy/org/kurron/spring/spock/SimpleIntegrationTest.groovy
+++ src/test/groovy/org/kurron/spring/spock/SimpleIntegrationTest.groovy
@@ -1,10 +1,18 @@
 package org.kurron.spring.spock
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+
 /**
  * Basic integration test.
  */
+@ContextConfiguration( "classpath:/org/kurron/spring/spock/SimpleIntegrationTest-context.xml" )
 class SimpleIntegrationTest extends AbstractIntegrationTest {
 
-    def 'bob'() {
+    @Autowired
+    String expected
+
+    def 'this can never pass because we generate random strings each run'() {
         given:
         String sut = Integer.toHexString( generator.nextInt( Integer.MAX_VALUE ) ).toUpperCase()
 
@@ -12,6 +20,6 @@ class SimpleIntegrationTest extends AbstractIntegrationTest {
         String upper = sut.toUpperCase()
 
         then:
-        assert upper.equals( 'BOB' )
+        assert upper.equals( expected )
     }
 }

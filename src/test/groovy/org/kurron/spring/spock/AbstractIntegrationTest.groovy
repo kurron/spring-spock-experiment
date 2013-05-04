659121ce50121deb501ae452d9b4ba606766e7c7
kurron <9gnp2a8egj>
2013-05-04 17:19:03 -0400

    Proved that the Spring context stuff can be moved into the base class.

--- src/test/groovy/org/kurron/spring/spock/AbstractIntegrationTest.groovy
+++ src/test/groovy/org/kurron/spring/spock/AbstractIntegrationTest.groovy
@@ -1,9 +1,16 @@
 package org.kurron.spring.spock
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
 /**
  * Base class for Spock-based integration tests.
  */
+@ContextConfiguration( "classpath:/org/kurron/spring/spock/AbstractIntegrationTest-context.xml" )
 abstract class AbstractIntegrationTest extends Specification {
+
+    @Autowired
+    protected Random generator
+
 }
--- src/test/groovy/org/kurron/spring/spock/SimpleIntegrationTest.groovy
+++ src/test/groovy/org/kurron/spring/spock/SimpleIntegrationTest.groovy
@@ -1,16 +1,8 @@
 package org.kurron.spring.spock
-
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.ContextConfiguration
-
 /**
  * Basic integration test.
  */
-@ContextConfiguration( "classpath:/org/kurron/spring/spock/SimpleIntegrationTest-context.xml" )
 class SimpleIntegrationTest extends AbstractIntegrationTest {
-
-    @Autowired
-    Random generator
 
     def 'bob'() {
         given:

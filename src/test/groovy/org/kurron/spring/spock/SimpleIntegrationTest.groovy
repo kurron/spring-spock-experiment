d4f91bf0c93d89dc828826f53774d3b90fe15d0d
kurron <9gnp2a8egj>
2013-05-04 18:05:25 -0400

    Showed how slf4j can work in Groovy.

--- src/test/groovy/org/kurron/spring/spock/SimpleIntegrationTest.groovy
+++ src/test/groovy/org/kurron/spring/spock/SimpleIntegrationTest.groovy
@@ -1,5 +1,7 @@
 package org.kurron.spring.spock
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 
@@ -9,15 +11,19 @@ import org.springframework.test.context.ContextConfiguration
 @ContextConfiguration( classes = SimpleIntegrationTestConfiguration.class  )
 class SimpleIntegrationTest extends AbstractIntegrationTest {
 
+    private static final Logger logger = LoggerFactory.getLogger( SimpleIntegrationTest.class )
+
     @Autowired
     String expected
 
     def 'this can never pass because we generate random strings each run'() {
         given:
         String sut = Integer.toHexString( generator.nextInt( Integer.MAX_VALUE ) ).toUpperCase()
+        logger.debug( 'sut = {}', sut )
 
         when:
         String upper = sut.toUpperCase()
+        logger.debug( 'upper = {}', upper )
 
         then:
         assert upper.equals( expected )
